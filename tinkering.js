8d7fd7792d2a6ea2a8b21ed8f3ceddbb205153db
David Curry <49762047+SkillProDave@users.noreply.github.com>
2021-03-21 23:31:38 -0300

    Add files via upload

---
+++ tinkering.js
@@ -0,0 +1,18 @@
+function multiply(a, b) {
+  return a * b;
+}
+
+const result = multiply(2, 4);
+
+console.log(result);
+
+// x = 10;
+// if (x == 5) {
+//   console.log(`yup, it's 5`);
+  
+//   } else if (x == 10) {
+//   console.log(`totally a 10`);
+  
+//   } else {
+//   console.log(`not our numbers`);
+//   }
